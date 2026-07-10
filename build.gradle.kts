69adb4ec5e8d2edd8dd9a5b0a7305160d94bd1e5
SherlockChiang <sherlockchiang@gmail.com>
2026-07-10 21:09:30 +0800

    Enable reproducible dependency resolution

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,23 +1,19 @@
-import java.io.ByteArrayOutputStream
-
 plugins {
     alias(libs.plugins.agp.app) apply false
 }
 
-fun String.execute(currentWorkingDir: File = file("./")): String {
-    val byteOut = ByteArrayOutputStream()
-    project.exec {
-        workingDir = currentWorkingDir
-        commandLine = split("\\s".toRegex())
-        standardOutput = byteOut
-    }
-    return String(byteOut.toByteArray()).trim()
-}
-
-val gitCommitCount = "git rev-list HEAD --count".execute().toInt()
+val gitCommitCount = providers.exec {
+    commandLine("git", "rev-list", "HEAD", "--count")
+}.standardOutput.asText.get().trim().toInt()
 
 val verName by extra("v1")
 val verCode by extra(gitCommitCount)
+
+subprojects {
+    dependencyLocking {
+        lockAllConfigurations()
+    }
+}
 
 tasks.register("Delete", Delete::class) {
     delete(rootProject.layout.buildDirectory)
